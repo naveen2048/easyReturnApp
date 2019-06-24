d49a4c03fb41ff46596057eceb1205e0668b009c
Naveen Meesala <naveen.2048@gmail.com>
2019-06-23 20:20:07 -0400

    updated code to show orderline items on selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonService } from './services/common.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'easyReturn App';
   showSide = false;
   data:any;
+  subscription:any;
 
   constructor(private router:Router,
     private params:ActivatedRoute,
     private commonService: CommonService){
-      commonService.sideSheet().subscribe(d =>  {
+      this.subscription = commonService.sideSheet().subscribe(d =>  {
         this.showSide = d.sideSheet;
         this.data = d.data;
-        console.log(this.data);
       });
     }
-
 
   ngOnInit(){ }
 
   closeSheet() {
     this.commonService.toggleSideSheet(false);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
